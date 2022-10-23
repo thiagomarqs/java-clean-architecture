f8fe6d9082a5b1f53bf536a00611156ebe564cb6
thiagomarqs <thiagodsmarques@hotmail.com>
2022-10-23 13:41:55 -0300

    StudentRepository.

---
+++ src/main/java/org/example/school/domain/student/StudentRepository.java
@@ -0,0 +1,13 @@
+package org.example.school.domain.student;
+
+import java.util.List;
+
+public interface StudentRepository {
+
+    void enroll(Student student);
+
+    Student findByCPF(CPF cpf);
+
+    List<Student> findAllEnrolledStudents();
+
+}
